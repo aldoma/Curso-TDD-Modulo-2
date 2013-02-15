9375f069fefe74e5d8d799a8914368b1b7933086
Alberto Dominguez Matamoros <aldoma@gmail.com>
2013-02-15 17:32:27 +0100

    Convertido el array interno de String[][] a char[][]. Debería ser mucho más eficiente.
    Change-Id: I451869262e6e4e9d022fb53faf512bde9100a3c6
    Signed-off-by: Alberto Dominguez Matamoros <aldoma@gmail.com>

--- src/main/java/org/aldoma/cursotdd/modulo2/ejercicio1/Escitala.java
+++ src/main/java/org/aldoma/cursotdd/modulo2/ejercicio1/Escitala.java
@@ -33,7 +33,7 @@ public class Escitala {
 	public String encrypt() {
 		if (isValid()) {//comprobamos la frase las caras etc
 			final int largo = frase.length() % caras == 0 ? frase.length() / caras : frase.length() / caras + 1; //Si el mensaje dividido entre las caras de la escitala da resto cero no sobrarían espacios
-			final String[][] escitala = new String[caras][largo]; //las caras representan las columnas de la escitala
+			final char[][] escitala = new char[caras][largo]; //las caras representan las columnas de la escitala
 			//el largo representa las filas
 
 			int pivote = 0; //nos servirá de pivote para señalar el caracter actual en la frase
@@ -41,7 +41,7 @@ public class Escitala {
 			for (int columna = 0; columna < largo; columna++) { //recorremos todas las filas DE CADA COLUMNA
 				for (int fila = 0; fila < caras; fila++) { //recorremos todas las columnas
 					if (pivote < frase.length()) { //Si no hemos recorrido toda la frase
-						escitala[fila][columna] = String.valueOf( frase.charAt( pivote ) ); //Almacenamos el caracter que toque dentro del Array
+						escitala[fila][columna] = frase.charAt( pivote ); //Almacenamos el caracter que toque dentro del Array
 						pivote++; //sumamos uno al pivote para apuntar al siguiente caracter de la cadena de texto a encrypt
 					}
 					else {
@@ -64,13 +64,13 @@ public class Escitala {
 	public String decrypt( final String frase2 ) {
 		if (isValid()) { //comprobamos la frase las caras etc
 			final int largo = frase.length() % caras == 0 ? frase.length() / caras : frase.length() / caras + 1; //Si el mensaje dividido entre las caras de la escitala da resto cero no sobrarían espacios
-			final String[][] escitala = new String[largo][caras];
+			final char[][] escitala = new char[largo][caras];
 
 			int pivote = 0; //Nos servirá de pivote para la frase
 
 			for (int columnas = 0; columnas < caras; columnas++) { //OJO esta vez la cantidad de columnas lo determinarán las caras
 				for (int filas = 0; filas < largo; filas++) { //OJO esta vez las la cantidad de filas vendrá determinada por el largo.
-					escitala[filas][columnas] = String.valueOf( frase2.charAt( pivote ) ); //vamos escribiendo caracter a caracter en el array bidimensional
+					escitala[filas][columnas] = frase2.charAt( pivote ); //vamos escribiendo caracter a caracter en el array bidimensional
 					pivote++; //avanzamos el pivote una posición para que apunte al siguiente caracter del String a convertir.
 				}
 			}
@@ -88,26 +88,24 @@ public class Escitala {
 	 * 
 	 * @return un carácter aleatorio.
 	 */
-	private static String aleatorio() {
+	private static char aleatorio() {
 		final int aleatorio = (int) (Math.random() * 100) + 1; //calculamos un aleatorio de 1 a 100. Podríamos haber pusto otro rango.
 
-		return String.valueOf( (char) aleatorio ); //Devolvemos como String el char que representa a ese número generado aleatorimente. (Código ASCII)
+		return (char) aleatorio; //Devolvemos como String el char que representa a ese número generado aleatorimente. (Código ASCII)
 	}
 
 	/**
 	 * Convierte el array bidimensional pasado como parámetro en un <code>String</code>.
 	 * 
-	 * @param array
+	 * @param escitala
 	 *        bidimensional a convertir en una cadena.
 	 * @return una cadena de texto representando el array pasado como argumento.
 	 */
-	private static String toText( final String[][] array ) {
+	private static String toText( final char[][] escitala ) {
 		final StringBuilder sb = new StringBuilder(); //Ya que no trabajamos con hilos creamos un StringBuilder que es más eficiente en estos casos
 
-		for (final String[] strings : array) {
-			for (final String string : strings) {
-				sb.append( string );
-			}
+		for (final char[] strings : escitala) {
+			sb.append( strings );
 		}
 		return sb.toString(); //Devolvemos como String el contenido del StringBuilder
 	}
